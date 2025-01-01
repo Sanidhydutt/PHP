b3178defccb552bd386e3d33c8d4ae536075ac64
SanidhyDutt <161296986+Sanidhydutt@users.noreply.github.com>
2025-01-01 12:18:25 +0530

    15_Traites_Overloading_2.php

--- oops/15_Traites_Overloading_2.php
+++ oops/15_Traites_Overloading_2.php
@@ -32,3 +32,37 @@ $a1->hello();
 
 $a1->newhell();
 ?>
+
+<?php 
+trait h1
+// hell
+{
+    public function hello()
+    {
+        echo("<br> Hello From H1 Trait");
+    }
+}
+
+trait h2
+// hyy
+{
+    public function hello()
+    {
+        echo("<br> Hello From H2 Trait");
+    }
+}
+
+class A
+{
+    use h1,h2
+    {
+        // h2::hello insteadOf h1;
+        // h1::hello as nh3;
+    }
+}
+$a1 = new A();
+
+$a1->hello();
+
+//$a1->nh3();
+?>
